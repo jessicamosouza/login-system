8eea1e071bef1cf7fb0caabd40533968cf3a46c6
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2023-11-21 17:11:57 -0300

    refatoring create_test.go

--- http/create_test.go
+++ http/create_test.go
@@ -3,6 +3,7 @@ package http
 import (
 	"bytes"
 	"errors"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -15,43 +16,27 @@ func (e *ErrorReader) Read([]byte) (n int, err error) {
 	return 0, errors.New("simulated read error")
 }
 
+const successfulBody = `{"fname":"John","lname":"Doe","email":"john@email.com","password":"Password123!"}`
+
 func TestCreateUser(t *testing.T) {
 	t.Run("Only POST method allowed", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodGet, "/", nil)
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-		rec := httptest.NewRecorder()
+		req, rec := createRequest(t, http.MethodGet, "/", "")
 		CreateUserHandler(rec, req)
-		if rec.Code != http.StatusMethodNotAllowed {
-			t.Errorf("expected status %d but got %d", http.StatusMethodNotAllowed, rec.Code)
-		}
+		checkResponseCode(t, rec, http.StatusMethodNotAllowed)
 	})
 
 	t.Run("Handle error reading request body", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/", &ErrorReader{})
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
+		errReader := io.NopCloser(bytes.NewReader(nil))
+		req, _ := http.NewRequest(http.MethodPost, "/", errReader)
 		rec := httptest.NewRecorder()
 		CreateUserHandler(rec, req)
-		if rec.Code != http.StatusInternalServerError {
-			t.Errorf("expected status %d but got %d", http.StatusInternalServerError, rec.Code)
-		}
+		checkResponseCode(t, rec, http.StatusInternalServerError)
 	})
 
 	t.Run("Successful body read", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/",
-			bytes.NewBufferString(`{"fname":"John","lname":"Doe","email":"john@email.com","password":"Password123!"}`))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-
-		rec := httptest.NewRecorder()
+		req, rec := createRequest(t, http.MethodPost, "/", successfulBody)
 		CreateUserHandler(rec, req)
-		if rec.Code != http.StatusOK {
-			t.Errorf("expected status %d but got %d", http.StatusOK, rec.Code)
-		}
+		checkResponseCode(t, rec, http.StatusOK)
 
 		resp := rec.Body.String()
 		if resp != "LoginUserPayload created successfully!" {
@@ -62,87 +47,96 @@ func TestCreateUser(t *testing.T) {
 }
 
 func TestEdgeCases(t *testing.T) {
-	t.Run("Invalid email", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/",
-			bytes.NewBufferString(`{"fname":"John","lname":"Doe","email":"john","password":"Password123!"}`))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
+	testCases := []struct {
+		name          string
+		requestBody   string
+		expectedCode  int
+		expectedError string
+	}{
+		{
+			"Invalid email",
+			`{"fname":"John","lname":"Doe","email":"john","password":"Password123!"}`,
+			http.StatusBadRequest,
+			"email validation failed: invalid email",
+		},
+		{
+			"Invalid password",
+			`{"fname":"John","lname":"Doe","email":"john@doe.com","password":"pass"}`,
+			http.StatusBadRequest,
+			"password validation failed: password does not meet the requirements",
+		},
+		{
+			"Missing field",
+			`{"fname":"John","lname":"Doe","password":"Password123!"}`,
+			http.StatusBadRequest,
+			"email validation failed: invalid email",
+		},
+		{
+			"Missing more than one field",
+			`{"fname":"John", "password":"pass"}`,
+			http.StatusBadRequest,
+			"email validation failed: invalid email",
+		},
+		{
+			"Invalid first name",
+			`{"fname":"J","lname":"Doe","email":"john@doe.com", "password":"Password123!"}`,
+			http.StatusBadRequest,
+			"first name validation failed: name must contain at least 2 characters",
+		},
+		{
+			"Invalid last name",
+			`{"fname":"John","lname":"D","email":"john@doe.com","password":"Password123!"}`,
+			http.StatusBadRequest,
+			"last name validation failed: name must contain at least 2 characters",
+		},
+		{
+			"Invalid password",
+			`{"fname":"John","lname":"Doe","email":"john@doe.com","password":"pass"}`,
+			http.StatusBadRequest,
+			"password validation failed: password does not meet the requirements",
+		},
+	}
 
-		rec := httptest.NewRecorder()
-		CreateUserHandler(rec, req)
-		if strings.TrimSpace(rec.Body.String()) != "email validation failed: invalid email" {
-			t.Errorf("unexpected error message: got %v want %v", rec.Body.String(), "email validation failed: invalid email")
-		}
-	})
-
-	t.Run("Invalid password", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/",
-			bytes.NewBufferString(`{"fname":"John","lname":"Doe","email":"john@doe.com","password":"pass"}`))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-
-		rec := httptest.NewRecorder()
-		CreateUserHandler(rec, req)
-		if strings.TrimSpace(rec.Body.String()) != "password validation failed: password does not meet the requirements" {
-			t.Errorf("unexpected error message: got %v want %v", rec.Body.String(), "password validation failed: password does not meet the requirements")
-		}
-	})
-
-	t.Run("Missing fields", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/",
-			bytes.NewBufferString(`{"fname":"John","lname":"Doe","password":"Password123!"}`))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-
-		rec := httptest.NewRecorder()
-		CreateUserHandler(rec, req)
-		if strings.TrimSpace(rec.Body.String()) != "email validation failed: invalid email" {
-			t.Errorf("unexpected error message: got %v want %v", rec.Body.String(), "email validation failed: invalid email")
-		}
-	})
-
-	t.Run("Invalid first name", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/",
-			bytes.NewBufferString(`{"fname":"J","lname":"Doe","email":"john@doe.com", "password":"Password123!"}`))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-
-		rec := httptest.NewRecorder()
-		CreateUserHandler(rec, req)
-		if strings.TrimSpace(rec.Body.String()) != "first name validation failed: name must contain at least 2 characters" {
-			t.Errorf("unexpected error message: got %v want %v", rec.Body.String(), "first name validation failed: name must contain at least 2 characters")
-		}
-	})
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, rec := createRequest(t, http.MethodPost, "/", tc.requestBody)
+			CreateUserHandler(rec, req)
+			checkResponseCode(t, rec, tc.expectedCode)
+			checkResponseMessage(t, rec, tc.expectedError)
+		})
+	}
 }
 
 func TestUnmarshalUser(t *testing.T) {
 	t.Run("Handle error unmarshalling request body", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString("Hello"))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-		rec := httptest.NewRecorder()
+		req, rec := createRequest(t, http.MethodPost, "/", "Hello")
 		CreateUserHandler(rec, req)
-		if rec.Code != http.StatusInternalServerError {
-			t.Errorf("expected status %d but got %d", http.StatusInternalServerError, rec.Code)
-		}
+		checkResponseCode(t, rec, http.StatusInternalServerError)
 	})
 
 	t.Run("Successful unmarshalling", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPost, "/",
-			bytes.NewBufferString(`{"fname":"John","lname":"Doe","email":"john@email.com","password":"Password123!"}`))
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-
-		rec := httptest.NewRecorder()
+		req, rec := createRequest(t, http.MethodPost, "/", successfulBody)
 		CreateUserHandler(rec, req)
-		if rec.Code != http.StatusOK {
-			t.Errorf("expected status %d but got %d", http.StatusOK, rec.Code)
-		}
+		checkResponseCode(t, rec, http.StatusOK)
 	})
+}
+
+func createRequest(t *testing.T, method, url, body string) (*http.Request, *httptest.ResponseRecorder) {
+	req, err := http.NewRequest(method, url, bytes.NewBufferString(body))
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	return req, httptest.NewRecorder()
+}
+
+func checkResponseCode(t *testing.T, rec *httptest.ResponseRecorder, expectedCode int) {
+	if rec.Code != expectedCode {
+		t.Errorf("expected status %d but got %d", expectedCode, rec.Code)
+	}
+}
+
+func checkResponseMessage(t *testing.T, rec *httptest.ResponseRecorder, expectedMessage string) {
+	if strings.TrimSpace(rec.Body.String()) != expectedMessage {
+		t.Errorf("unexpected error message: got %v want %v", rec.Body.String(), expectedMessage)
+	}
 }
